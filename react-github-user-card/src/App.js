962331bf7de1e21b2ab26818d7e3fa06bbd8a145
Jensen Koch <jtkoch12@gmail.com>
2020-02-13 21:21:33 -0600

    made api call with componentDidMount

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
+import UserCard from './components/UserCard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    user: {}
+  }
+
+  componentDidMount() {
+    axios 
+      .get('https://api.github.com/users/Jensen-Koch')
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          ...this.state,
+          user: res.data
+        })
+      })
+      .catch(err => console.log('ERROR', err))
+  }
+
+  
+
+
+render() {
+    return (
+      <div className="App">
+        <h1>Github-User-Card</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
