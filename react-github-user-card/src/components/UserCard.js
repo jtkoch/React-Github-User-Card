52128eb72a55a83498cb5920b4d42810ccb746f3
Jensen Koch <jtkoch12@gmail.com>
2020-03-01 19:58:34 -0600

    updating styling

--- react-github-user-card/src/components/UserCard.js
+++ react-github-user-card/src/components/UserCard.js
@@ -5,7 +5,7 @@ const UserCard = ({ user }) => {
       <div className="user-card">
          <img className="user-card-image" src={user.avatar_url} alt='Jensen Koch' />
          <h2>{user.name}</h2>
-         <h3>{user.bio}</h3>
+         <h3 className="bio">{user.bio}</h3>
          <h3 className="location">{user.location}</h3>
       </div>
    )
